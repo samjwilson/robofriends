ce3250a7627369214a6aff741ef821da948a81c9
samjwilson <samueljwilson@yahoo.com>
2018-03-26 15:49:50 -0600

    functional website with react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-import { robots } from './robots';
+
+
 
 ReactDOM.render(
-            <CardList robots={robots} />
+          <App />
 , document.getElementById('root'));
 registerServiceWorker();
